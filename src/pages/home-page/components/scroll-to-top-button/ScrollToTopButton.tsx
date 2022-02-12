8d198624395ce5f06bf68e2ab2664a60c468c6d6
DharmendraKumar Jagodana <jagodana.dharmendra@gmail.com>
2022-02-12 16:14:14 +0530

    Feat: add scroll-to-top button

---
+++ src/pages/home-page/components/scroll-to-top-button/ScrollToTopButton.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import { FiArrowUp } from "react-icons/fi";
+
+function ScrollToTopButton() {
+  const [visible, setVisible] = useState(false);
+
+  const toggleVisible = () => {
+    const scrolled = document.documentElement.scrollTop;
+    if (scrolled > 300) {
+      setVisible(true);
+    } else if (scrolled <= 300) {
+      setVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth", //auto
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisible);
+    return () => {
+      window.removeEventListener("scroll", toggleVisible);
+    };
+  });
+
+  return (
+    <>
+      <button
+        type="button"
+        className="bg-primary font-medium rounded-lg text-sm px-4 py-5 text-center mr-2 mb-2"
+        onClick={scrollToTop}
+        style={{ display: visible ? "inline" : "none" }}
+      >
+        <FiArrowUp className="h-4 w-4" />
+      </button>
+    </>
+  );
+}
+
+export default ScrollToTopButton;
