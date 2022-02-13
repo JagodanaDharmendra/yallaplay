3a6c5423936b73c379bc041a3db9b33d01a8445f
DharmendraKumar Jagodana <47411494+JagodanaDharmendra@users.noreply.github.com>
2022-02-13 11:05:53 +0530

    Fix: solve background issue

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,4 @@
 export * from "./appicon";
 export * from "./navbar";
-export * from "./background";
 export * from "./footer";
 export * from "./container";
--- src/pages/home-page/Home.tsx
+++ src/pages/home-page/Home.tsx
@@ -1,4 +1,4 @@
-import { Background, Container, Footer, Navbar } from "../../components";
+import { Container, Footer, Navbar } from "../../components";
 import {
   AboutUsView,
   BannerView,
@@ -12,64 +12,71 @@ import {
 
 function Home() {
   return (
-    <div className="relative text-white">
-      {/* Background */}
-      <Background />
+    <div className="text-white bg-cover bg-no-repeat bg-center" style={{ backgroundImage: `url(${require("../../assets/bg-site.jpeg")})` }}>
+
+      {/* Nav Bar */}
+      {
+        <>
+          < Container >
+            <Navbar />
+          </Container >
+        </>
+      }
 
       {/* Content */}
-      <div className="absolute top-0 left-0 w-screen">
-        {/* Nav Bar */}
-        <Container>
-          <Navbar />
-        </Container>
+      {
+        <>
+          < Container >
+            <BannerView />
+          </Container >
 
-        {/* Content */}
-        <Container>
-          <BannerView />
-        </Container>
+          <Container>
+            <div id="about-us">
+              <AboutUsView />
+            </div>
+          </Container>
+          <Container>
+            <div id="our-games">
+              <OurGamesView />
+            </div>
+          </Container>
+          <Container>
+            <div id="our-services">
+              <OurServicesView />
+            </div>
+          </Container>
+          <Container>
+            <div id="founders">
+              <TeamView />
+            </div>
+          </Container>
+          <Container>
+            <div id="investors">
+              <InvestorsView />
+            </div>
+          </Container>
+          <Container>
+            <div id="contact-us">
+              <ContactUsView />
+            </div>
+          </Container>
 
-        <Container>
-          <div id="about-us">
-            <AboutUsView />
-          </div>
-        </Container>
-        <Container>
-          <div id="our-games">
-            <OurGamesView />
-          </div>
-        </Container>
-        <Container>
-          <div id="our-services">
-            <OurServicesView />
-          </div>
-        </Container>
-        <Container>
-          <div id="founders">
-            <TeamView />
-          </div>
-        </Container>
-        <Container>
-          <div id="investors">
-            <InvestorsView />
-          </div>
-        </Container>
-        <Container>
-          <div id="contact-us">
-            <ContactUsView />
-          </div>
-        </Container>
-
-        {/* Footer */}
-        <Container>
-          <Footer />
-        </Container>
-      </div>
+          {/* Footer */}
+          <Container>
+            <Footer />
+          </Container>
+        </>
+      }
 
       {/* Scroll to Top Button */}
-      <div className="fixed right-5 bottom-5">
-        <ScrollToTopButton />
-      </div>
-    </div>
+      {
+        <>
+          <div className="fixed right-5 bottom-5" >
+            <ScrollToTopButton />
+          </div>
+        </>
+      }
+    </div >
   );
 }
 
