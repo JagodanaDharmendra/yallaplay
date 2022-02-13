51bb6e748766d70bb0db7d7150c8bf6f4637099a
DharmendraKumar Jagodana <47411494+JagodanaDharmendra@users.noreply.github.com>
2022-02-13 13:00:52 +0530

    Feat: add mobile menu and blur effect

--- src/components/container/Container.tsx
+++ src/components/container/Container.tsx
@@ -1,8 +1,8 @@
 function Container(props: any) {
   return (
-    <div className="flex w-full items-center content-center justify-center">
-      <div className="container flex">
-        <div className="w-full px-4">{props.children}</div>
+    <div className="container">
+      <div className="px-6 md:px-8">
+        {props.children}
       </div>
     </div>
   );
